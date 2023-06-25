f3d4fed88323b101ea094499d646d2ddbaf07b35
KeshavSehgal01 <87082895+KeshavSehgal01@users.noreply.github.com>
2023-06-25 23:11:15 +0530

    Add files via upload

---
+++ src/Components/Transaction.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { GlobalContext } from "../Context/GlobleState";
+
+const Transaction = ({ transactions }) => {
+  const { deletTransanction } = useContext(GlobalContext);
+  const sign = transactions.amount < 0 ? "-" : "+";
+
+  return (
+    <li className={transactions.amount < 0 ? "minus" : "plus"}>
+      {transactions.text}
+      <span>
+        {sign}₹{Math.abs(transactions.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deletTransanction(transactions.id)}>
+        x
+      </button>
+    </li>
+  );
+};
+
+export default Transaction;
